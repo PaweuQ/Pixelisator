b2b15efb31fbc55b388dc5e38ad2a79bd7a9f7ba
Paweł Bartkiewicz <122686426+Hexyr7@users.noreply.github.com>
2026-01-26 22:12:31 +0100

    Add files via upload

---
+++ software/learning/learning_4/SceneManager.cpp
@@ -0,0 +1,39 @@
+// Author:  Paweł Bartkiewicz 
+// email:   15g.pawel.bart@gmail.com
+// github:  Hexyr7
+// licence: MIT
+
+#include "SceneManager.h"
+#include "BlinkScene.h"
+#include "BlueScene.h"
+
+void SceneManager::setScene(Scene* next) {
+    if (current) {   //shortcut of if (current != nullptr)
+      current->onExit();
+      delete current;
+    }
+    current = next;
+    current->onEnter(); 
+}
+
+void SceneManager::update() {
+  if (!current) return;
+
+  current->update();
+
+  if (current->nextScene != SceneID::None) {
+    SceneID next = current->nextScene;
+    current->nextScene = SceneID::None;
+
+    switch(next){
+      case SceneID::Blink:
+        setScene(new BlinkScene());
+        break;
+      case SceneID::Blue:
+        setScene(new BlueScene());
+        break;
+      default:
+        break;
+    }
+  }
+}
