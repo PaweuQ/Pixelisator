1b3a15ac3b5460a1e05c8644e80bcd63ba2522e4
Paweł Bartkiewicz <122686426+Hexyr7@users.noreply.github.com>
2026-01-26 22:11:26 +0100

    Add files via upload

---
+++ software/learning/learning_4/BlinkScene.h
@@ -0,0 +1,19 @@
+// Author:  Paweł Bartkiewicz 
+// email:   15g.pawel.bart@gmail.com
+// github:  Hexyr7
+// licence: MIT
+
+#pragma once
+#include <Arduino.h>
+#include "Scene.h"
+
+class BlinkScene : public Scene {
+public:
+  void onEnter() override;
+  void onExit() override;
+  void update() override;
+
+private:
+  uint32_t lastToggle = 0;
+  bool state = false;
+};
